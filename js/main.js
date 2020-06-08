188db8b5562bff94778530cfea0c09689cff8c12
Argish Arakelyan <argishtarakelyan@gmail.com>
2020-06-08 16:16:55 +0400

    ...

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+
+ $(document).ready(function() {
+//     $('body').css('padding-bottom', $('footer').height() + 32);
+
+
+$('.casestudy-wrap .slider_wrap').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    items:1,
+});
+$(".owl-prev").html('<i class="fa fa-chevron-left"></i>');
+$(".owl-next").html('<i class="fa fa-chevron-right"></i>');
+});
+
+// $(window).resize(function(){
+//     $('body').css('padding-bottom', $('footer').height() + 32);
+// });
+
+// $(document).on("click","",function() {
+ 
+// }); 
+
+
